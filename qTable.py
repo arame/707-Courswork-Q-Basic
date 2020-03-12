58c558206e8f4d9038528ef1d23623f3297217bc
Geoffrey Payne <hijiki7777@yahoo.co.uk>
2020-03-12 08:41:10 +0000

    Might be working now

--- qTable.py
+++ qTable.py
@@ -9,22 +9,28 @@ class QTable:
     def __init__(self, rtable):
         # Initialise the Q_value table to equal that of the rewards table, set to zero
         self.Q_Table = np.zeros(rtable.shape)
+        self.I1 = State(1, 1)
+        self.I2 = State(1, 3)
+        self.I3 = State(3, 0)
         print("Q table created")
 
     def update(self, newState, oldState, reward):
         alpha = Hyperparam.learning_rate
         gamma = Hyperparam.discount_factor
-        old = ConfigTable.getIndexForDirtyCellState(oldState)
-        new = ConfigTable.getCellIndex(newState)
-        q_old = self.Q_Table[old, new]
-        self.q_max = np.amax(self.Q_Table)
+        _state = ConfigTable.getIndexForDirtyCellState(oldState)
+        _action = ConfigTable.getCellIndex(newState)
+        q_old = self.Q_Table[_state, _action]
+        self.getMaxQAction(newState)
+        #self.q_max = np.max(self.Q_Table[_state_next:])
+        self.q_max = self.bestQ
         q_new = q_old + alpha * (reward + gamma * self.q_max - q_old)
+        # Q[s_old,a] = Q[s_old,a] + alpha * ( r + gamma * np.max(Q[s,:]) - Q[s_old,a])
         # print("-"*50)
         # print(f"Old Q = ({oldState.row}, {oldState.column}), index_old = {index_old}, old Value = {q_old}")
         # print(f"Update New Q = ({newState.row}, {newState.column}), index_new = {index_new}, new value = {q_new}, reward = {reward}")
-        self.Q_Table[old, new] = q_new
-    
-    def getBestQvalue(self, state):
+        self.Q_Table[_state, _action] = q_new
+
+    def getMaxQAction(self, state):
         x = state.row
         y = state.column
         
@@ -34,9 +40,13 @@ class QTable:
         self.getNextQValue(state, State(x + 1, y))
         self.getNextQValue(state, State(x, y - 1))
         self.getNextQValue(state, State(x, y + 1))
+    
+    def getBestQvalue(self, state):
+        self.getMaxQAction(state)
         #print(f"Best Q = {self.bestQ} ({self.bestState.row}, {self.bestState.column})")
         if self.bestQ == 0:
             return state
+
         return self.bestState
 
     def getNextQValue(self, state, nextState):
