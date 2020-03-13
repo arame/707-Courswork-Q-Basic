0e618c45243516db9176146c0036f418c8f9b35f
Geoffrey Payne <hijiki7777@yahoo.co.uk>
2020-03-13 18:56:28 +0000

    Minor changes

--- hyperparameters.py
+++ hyperparameters.py
@@ -3,8 +3,8 @@
 class Hyperparam:
     epsilon = 0.9
     epsilon_decay = 0.9999
-    learning_rate = 0.5
-    discount_factor = 0.5
+    learning_rate = 0.9
+    discount_factor = 0.99
     noOfEpisodes = 5000
 
     @staticmethod
